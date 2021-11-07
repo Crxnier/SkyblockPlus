7871c31ccc41894bb7cf6bbf1b74f7ff5a7b8200
YosephGordon <superminecraftdrago@hotmail.com>
2021-11-07 12:40:41 +0000

    Allows for multiple users to be added

--- commands/eval.js
+++ commands/eval.js
@@ -2,21 +2,20 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   try{
-    if(message.author.id !== "157619634504204289") return message.channel.send("❌ Sorry only the bot owner can do this ❌");
-
+    IDs = ["157619634504204289","799004770148286504"]
+    if(!IDs.includes(message.author.id)) return message.channel.send("❌ Sorry only the bot owner can do this ❌");
     function clean(text) {
   if (typeof(text) === "string")
     return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
   else
-      return text;
+    return text;
 }
   try {
-      const code = args.join(" ");
-      let evaled = eval(code);
+    const code = args.join(" ");
+    let evaled = eval(code);
 
-      if (typeof evaled !== "string")
-        evaled = require("util").inspect(evaled);
-
+    if (typeof evaled !== "string")
+      evaled = require("util").inspect(evaled);
       message.channel.send(clean(evaled), {code:"xl"});
     } catch (err) {
       throw err
