5b3e66be04c2d06bedb208bee69181f2700fd47a
YosephGordon <superminecraftdrago@hotmail.com>
2021-11-07 12:40:06 +0000

    Fixed uptime, accidently pushed an old version (from old bot)

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,23 +1,25 @@
 const Discord = require("discord.js");
-const bot = new Discord.Client
+const randomColor = ((1 << 24) * Math.random() | 0).toString(16);
+
 
 module.exports.run = async (bot, message, args) => {
 try{
-  let color = ((1 << 24) * Math.random() | 0).toString(16);
 
-//Maths to get bot uptime
-    let totalSeconds = (bot.uptime / 1000);
-    let hours = Math.floor(totalSeconds / 3600);
-    totalSeconds %= 3600;
-    let minutes = Math.floor(totalSeconds / 60);
-    let seconds = Math.floor(totalSeconds / 60);
-//Making the result
-    let uptime = `${hours} hours, ${minutes} minutes and ${seconds} seconds`;
-//Embeding and sending message
-    let uEmbed = new Discord.RichEmbed()
-    .setColor(`#${color}`)
-    .setDescription(`**${uptime}**`);
-    message.channel.send(uEmbed);
+
+  let totalSeconds = (bot.uptime / 1000);
+  let days = Math.floor(totalSeconds / 86400);
+  totalSeconds %= 86400;
+  let hours = Math.floor(totalSeconds / 3600);
+  totalSeconds %= 3600;
+  let minutes = Math.floor(totalSeconds / 60);
+  let seconds = Math.floor(totalSeconds % 60);
+
+  let uptime = `${hours} hours, ${minutes} minutes and ${seconds} seconds`;
+
+  const uptimeEmbed = new Discord.MessageEmbed()
+  .setColor(randomColor)
+  .setDescription(`**${uptime}**`);
+  message.channel.send(uptimeEmbed);
 }catch(e){
   console.log(e.stack)
   message.channel.send("Sorry! I ran into an error while executing the command.")
